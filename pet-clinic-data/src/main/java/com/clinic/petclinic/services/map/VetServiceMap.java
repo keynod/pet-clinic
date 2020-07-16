9741e0b57bf6951913473c453da8ddd2e40d7244
Sergiy <srg.gorbachov@gmail.com>
2020-07-16 16:24:23 +0300

    Adding bootstrap for data (map). closes #16

--- pet-clinic-data/src/main/java/com/clinic/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.clinic.petclinic.services.map;
 
 import com.clinic.petclinic.model.Vet;
-import com.clinic.petclinic.services.CrudService;
+import com.clinic.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
