3cd332cb8070158bce6e105bd64a82f7dfe59575
Serhii Hor <srg.gorbachov@gmail.com>
2020-11-10 23:04:29 +0200

    Write CRUD tests for Owner Map Service. Closes #67

--- pet-clinic-data/src/main/java/com/clinic/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/clinic/petclinic/model/Owner.java
@@ -1,8 +1,16 @@
 package com.clinic.petclinic.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,9 +35,9 @@ public class Owner extends Person {
     private Set<Pet> pets = new HashSet<>();
 
     @Builder
-    public Owner(String firstName, String lastName, String address, String city, String telephone,
+    public Owner(Long id,  String firstName, String lastName, String address, String city, String telephone,
                  Set<Pet> pets) {
-        super(firstName, lastName);
+        super(id, firstName, lastName);
         this.address = address;
         this.city = city;
         this.telephone = telephone;
